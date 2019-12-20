6d6949c7f61fc97e676b11908672e286c3363a30
properTalk <auin1929@naver.com>
2019-12-20 15:43:09 +0900

    commit test

--- src/main/java/ktds/fresh/kafkaredisexample/KafkaRedisExampleApplication.java
+++ src/main/java/ktds/fresh/kafkaredisexample/KafkaRedisExampleApplication.java
@@ -8,7 +8,7 @@ public class KafkaRedisExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaRedisExampleApplication.class, args);
-        System.out.println("test");
+        System.out.println("test2222");
 
 
     }
